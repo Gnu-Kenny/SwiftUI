4087623130c5e19df969d7a52c427645844c53b1
Geunwoo Park <qkr0677@gmail.com>
2021-04-14 22:54:44 +0900

    Update-MyProfile-scrollView

--- stack_practice_tutorial/stack_practice_tutorial/MyProfile.swift
+++ stack_practice_tutorial/stack_practice_tutorial/MyProfile.swift
@@ -10,56 +10,69 @@ import SwiftUI
 struct MyProfile:View{
     var body: some View{
         NavigationView{
-            VStack{
-                Image("Profile")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    .clipShape(Circle())
-                    .shadow(color:Color.gray,radius: 15, x:5,y:5)
-                    .overlay(
-                        Circle()
-                            .stroke(Color.purple, lineWidth: 10)
-                            
-                            
-                    )
-                    .padding()
-                
-                Text("Gnu-Kenny")
-                    .font(.system(size: 40))
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .padding(3)
-                
-                Text("Click the below button")
-                    .font(.system(size:25))
-                    .fontWeight(.bold)
-                    .padding(.bottom, 20)
-                HStack(spacing: 30){
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Github")
-                            .font(.system(size: 30))
-                            .foregroundColor(.white)
-                            .fontWeight(.heavy)
-                            .padding(10)
-                            .background(Color.black)
-                            .cornerRadius(15)
-                            .shadow(color:Color.gray,radius: 15, x:5,y:5)
-                    })
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Velog")
-                            .font(.system(size: 30))
-                            .foregroundColor(.white)
-                            .fontWeight(.heavy)
-                            .padding(10)
-                            .padding(.horizontal,10)
-                            .background(Color.green)
-                            .cornerRadius(15)
-                            .shadow(color:Color.gray,radius: 15, x:5,y:5)
-                    })
+//            NavigationLink(
+//                destination: Text("넘겨진 화면입니다")){
+//                Image(systemName: "gear")
+//                    .font(.largeTitle)
+//                    .foregroundColor(Color.black)
+//            }.frame(maxWidth: .infinity, alignment: .trailing)
+            ScrollView{
                     
+                        VStack{
+                            
+                            Image("Profile")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .clipShape(Circle())
+                                .shadow(color:Color.gray,radius: 15, x:5,y:5)
+                                .overlay(
+                                    Circle()
+                                        .stroke(Color.purple, lineWidth: 10)
+                                )
+                                .padding()
+                            
+                            Text("Gnu-Kenny")
+                                .font(.system(size: 40))
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .padding(3)
+                            
+                            Text("Click the below button!")
+                                .font(.system(size:25))
+                                .fontWeight(.bold)
+                                .padding(.bottom, 20)
+                            
+                            HStack(spacing: 30){
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    Text("Github")
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.white)
+                                        .fontWeight(.heavy)
+                                        .padding(10)
+                                        .background(Color.black)
+                                        .cornerRadius(15)
+                                        .shadow(color:Color.gray,radius: 15, x:5,y:5)
+                                })
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    Text("Velog")
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.white)
+                                        .fontWeight(.heavy)
+                                        .padding(10)
+                                        .padding(.horizontal,10)
+                                        .background(Color.green)
+                                        .cornerRadius(15)
+                                        .shadow(color:Color.gray,radius: 15, x:5,y:5)
+                                })
+                                
+                            }
+                            .navigationBarTitle("My Profile")
+                            
+                            //.navigationTitle("My Profile")
+                            //
+                         
+                            
+                        } // VStack
                 }
-             
-                
-            }
             
         }
         
