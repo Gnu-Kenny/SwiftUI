eb8cf7e41849aedaa44c3b758cc82a909c68e3a4
Geunwoo Park <qkr0677@gmail.com>
2021-04-18 13:21:19 +0900

    Create ContentView.swift

---
+++ QRCode_Reader_SwiftUI_Tutorial/QRCode_Reader_SwiftUI_Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCode_Reader_SwiftUI_Tutorial
+//
+//  Created by 박근우 on 2021/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
