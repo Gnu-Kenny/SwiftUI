d3d927f6db0d05e3870a03e4b7b70a72ad417fa0
Geunwoo Park <qkr0677@gmail.com>
2021-05-02 18:16:25 +0900

    separate-ButtonStyle

--- ButtonStyle_SwiftUI/ButtonStyle_SwiftUI/MyButtonStyle.swift
+++ ButtonStyle_SwiftUI/ButtonStyle_SwiftUI/MyButtonStyle.swift
@@ -7,8 +7,14 @@
 
 import SwiftUI
 
+enum ButtonType {
+    case tab
+    case long
+}
+
 struct MyButtonStyle : ButtonStyle {
-    
+    var color : Color
+    var type : ButtonType
     // configuration에 매개 변수 들어오게 됨. 해당하는 스타일로 만들어지는 버튼
     func makeBody(configuration: Configuration) -> some View {
         // label -> 텍스트
@@ -16,69 +22,25 @@ struct MyButtonStyle : ButtonStyle {
             .padding()
             .font(.system(size:20))
             .foregroundColor(.white)
-            .background(Color.blue)
+            .background(color)
             .cornerRadius(15)
             .scaleEffect(configuration.isPressed ? 1.3:1.0)
+            .onTapGesture {
+                if(self.type == .tab) {
+                    let haptic = UIImpactFeedbackGenerator(style: .light)
+                    haptic.impactOccurred()
+                }
+            }.onLongPressGesture {
+                if(self.type == .long) {
+                    let haptic = UIImpactFeedbackGenerator(style: .heavy)
+                    haptic.impactOccurred()
+                }
+            }
         
     }
 }
 
-struct MyLongButtonStyle : ButtonStyle {
-    // configuration에 매개 변수 들어오게 됨. 해당하는 스타일로 만들어지는 버튼
-    func makeBody(configuration: Configuration) -> some View {
-        // label -> 텍스트
-        configuration.label
-            .padding()
-            .font(.system(size:20))
-            .foregroundColor(.white)
-            .background(Color.green)
-            .cornerRadius(15)
-            .scaleEffect(configuration.isPressed ? 1.3:1.0)
-    }
-}
 
-struct MyMinButtonStyle : ButtonStyle {
-    // configuration에 매개 변수 들어오게 됨. 해당하는 스타일로 만들어지는 버튼
-    func makeBody(configuration: Configuration) -> some View {
-        // label -> 텍스트
-        configuration.label
-            .padding()
-            .font(.system(size:20))
-            .foregroundColor(.white)
-            .background(Color.purple)
-            .cornerRadius(15)
-            .scaleEffect(configuration.isPressed ? 0.7:1.0)
-    }
-}
-
-struct MyRotateButtonStyle : ButtonStyle {
-    // configuration에 매개 변수 들어오게 됨. 해당하는 스타일로 만들어지는 버튼
-    func makeBody(configuration: Configuration) -> some View {
-        // label -> 텍스트
-        configuration.label
-            .padding()
-            .font(.system(size:20))
-            .foregroundColor(.white)
-            .background(Color.red)
-            .cornerRadius(15)
-            .rotationEffect(configuration.isPressed ? Angle(degrees: 180) : Angle(degrees: 0) )
-    }
-}
-
-struct MyBlurButtonStyle : ButtonStyle {
-    // configuration에 매개 변수 들어오게 됨. 해당하는 스타일로 만들어지는 버튼
-    func makeBody(configuration: Configuration) -> some View {
-        // label -> 텍스트
-        configuration.label
-            .padding()
-            .font(.system(size:20))
-            .foregroundColor(.white)
-            .background(Color.black)
-                .blur(radius: configuration.isPressed ? 3.0 : 0.0)
-            .cornerRadius(15)
-            
-    }
-}
 
 
 struct MyButtonStyle_Previews:PreviewProvider {
@@ -88,6 +50,6 @@ struct MyButtonStyle_Previews:PreviewProvider {
             }, label: {
                 Text("호호")
             }
-        ).buttonStyle(MyRotateButtonStyle())
+        ).buttonStyle(MyButtonStyle(color: Color.blue, type: .tab))
     }
 }
