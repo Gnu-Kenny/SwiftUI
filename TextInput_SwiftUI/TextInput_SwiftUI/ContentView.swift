fe64365abe7a2e4aa324b6b272d23c85eee2ceef
Geunwoo Park <qkr0677@gmail.com>
2021-05-04 20:37:26 +0900

    Update-ContentView-solution

--- TextInput_SwiftUI/TextInput_SwiftUI/ContentView.swift
+++ TextInput_SwiftUI/TextInput_SwiftUI/ContentView.swift
@@ -8,24 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputValue:String = ""
-    @State private var inputPassword:String = ""
+    @State private var username:String = ""
+    @State private var password:String = ""
     var body: some View {
-        GeometryReader{ geometry in
+        GeometryReader{ geometryProxy in
             VStack(spacing:10){
-                TextField("아이디를 입력하시오", text: $inputValue )
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .border(Color(UIColor.separator))
-                    .frame(width:geometry.size.width/10*9, height: 10)
-                SecureField("패스워드를 입력하시오", text: $inputPassword)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .border(Color(UIColor.separator))
-                    .frame(width:geometry.size.width/10*9, height: 10)
+                HStack{
+                    TextField("아이디를 입력하시오", text: $username )
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .border(Color(UIColor.separator))
+                    Button(action: {
+                        self.username = ""
+                    }, label: {
+                        if(self.username.count > 0){
+                            Image(systemName: "multiply.circle.fill")
+                                .font(.system(size: 25))
+                                .foregroundColor(.secondary)
+                        }
+                        
+                    })
+                }
+                HStack{
+                    SecureField("패스워드를 입력하시오", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .border(Color(UIColor.separator))
+                    Button(action: {
+                        self.password = ""
+                    }, label: {
+                        if(self.password.count > 0){
+                            Image(systemName: "multiply.circle.fill")
+                                .font(.system(size: 25))
+                                .foregroundColor(.secondary)
+                        }
+                    })
+                }
                 
-                Text("비밀번호: \(inputPassword)")
-            }
+                
+                Text("비밀번호: \(password)")
+            }.padding()
+            .position(x:geometryProxy.size.width/2, y:geometryProxy.size.height/2)
         }
         
         
